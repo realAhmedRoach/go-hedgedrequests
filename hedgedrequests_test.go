883ae6b34c45827291539dd7c919af5d47f87fc9
Ahmed Roach <realahmedroach@gmail.com>
2020-09-27 14:50:19 -0400

    Basic test

--- hedgedrequests_test.go
+++ hedgedrequests_test.go
@@ -1,1 +1,20 @@
 package hedgedrequests_test
+
+import (
+	. "hedgedrequests"
+	"testing"
+)
+
+// Test if the result of a simple function
+// is returned
+func TestHedgedRequest_Simple(t *testing.T) {
+	req := func() []byte {
+		return []byte("test")
+	}
+
+	res := HedgedRequest(req, 100, 1)
+
+	if string(res) != "test" {
+		t.Fatal("simple request failed")
+	}
+}
